9773f7346cc9d3a6b6e3a544ed547550eb5bb843
Ember Bland <gdnghtgrl@gmail.com>
2020-12-27 17:32:09 -0800

    add individual margin editing

--- extension_scenes/percentmargincontainer/percentmargincontainer.gd
+++ extension_scenes/percentmargincontainer/percentmargincontainer.gd
@@ -2,33 +2,91 @@ tool
 
 extends Container
 
-export (float, 0.0, 1.0, 0.001) var horizontal_size = 1.0 setget edit_horizontal_size
-export (float, 0.0, 1.0, 0.001) var vertical_size = 1.0 setget edit_vertical_size
+export (float, 0.0, 1.0, 0.001) var horizontal_margin = 1.0 setget edit_horizontal_margin
+export (float, 0.0, 1.0, 0.001) var vertical_margin = 1.0 setget edit_vertical_margin
 
-func edit_horizontal_size(input:float):
-	horizontal_size = input
+export var use_individual_margins: bool = false setget edit_use_indie_margins
+
+export var individual_margins: Dictionary = {
+	"Left":1.0,
+	"Right":1.0,
+	"Top":1.0,
+	"Bottom":1.0,
+} setget edit_individual_margins
+
+enum editing {
+	H,
+	V,
+}
+var currently_editing: int = editing.H # to allow h or v to be adjusted without overwriting the other's individual margins
+
+func _ready():
+	print(individual_margins)
+
+func edit_horizontal_margin(input:float):
+	horizontal_margin = input
+	currently_editing = editing.H
 	_on_sort_children()
 
-func edit_vertical_size(input:float):
-	vertical_size = input
+func edit_vertical_margin(input:float):
+	vertical_margin = input
+	currently_editing = editing.V
 	_on_sort_children()
+
+func edit_use_indie_margins(edit:bool):
+	use_individual_margins = edit
+	if edit:
+		edit_individual_margins(individual_margins)
+	else:
+		_on_sort_children()
+
+func edit_individual_margins(new_margins:Dictionary):
+	var old_margins = individual_margins
+	individual_margins["Left"] = new_margins["Left"]
+	individual_margins["Top"] = new_margins["Top"]
+	individual_margins["Right"] = new_margins["Right"]
+	individual_margins["Bottom"] = new_margins["Bottom"]
+	use_individual_margins = true
+	var child_count = get_child_count()
+	for i in child_count:
+		var current_child = get_child(i)
+		var left_movement = lerp(rect_size.x, 0.0, individual_margins["Left"])
+		var top_movement = lerp(rect_size.y, 0.0, individual_margins["Top"])
+		var right_movement = lerp(rect_size.x, 0.0, individual_margins["Right"])
+		var bottom_movement = lerp(rect_size.y, 0.0, individual_margins["Bottom"])
+		current_child.margin_right = -right_movement + (right_movement/2)
+		current_child.margin_left = left_movement - (left_movement/2)
+		current_child.margin_top = top_movement - (top_movement/2)
+		current_child.margin_bottom = -bottom_movement + (bottom_movement/2)
+		continue
+	print("Old: %s\nNew: %s" % [old_margins, new_margins])
+	if (old_margins["Left"] != new_margins["Left"]) or (old_margins["Right"] != new_margins["Right"]):
+		currently_editing = editing.H
+		print("H!")
+	if (old_margins["Top"] != new_margins["Top"]) or (old_margins["Bottom"] != new_margins["Bottom"]):
+		currently_editing = editing.V
+		print("V!")
 
 func _on_sort_children():
 	var child_count = get_child_count()
+	use_individual_margins = false
 	for i in child_count:
-		get_child(i).set_anchors_preset(Control.PRESET_WIDE)
-		get_child(i).set_margins_preset(Control.PRESET_WIDE) # might not need this
-		var full_size = rect_size
-		var horizontal_movement = lerp(rect_size.x, 0.0, horizontal_size)
-		var vertical_movement = lerp(rect_size.y, 0.0, vertical_size)
-		# todo: replace $ with get_child(i)
-		# todo: add 2nd scenetype that lets you do individual sides
-		$ColorRect.rect_size.x = lerp(0.0, rect_size.x, horizontal_size)
-		$ColorRect.rect_size.y = lerp(0.0, rect_size.y, vertical_size)
-		$ColorRect.margin_right = -horizontal_movement + (horizontal_movement/2)
-		$ColorRect.margin_left = horizontal_movement - (horizontal_movement/2)
-		$ColorRect.margin_top = vertical_movement - (vertical_movement/2)
-		$ColorRect.margin_bottom = -vertical_movement + (vertical_movement/2)
+		var current_child = get_child(i)
+		current_child.set_anchors_preset(Control.PRESET_WIDE)
+		var horizontal_movement = lerp(rect_size.x, 0.0, horizontal_margin)
+		var vertical_movement = lerp(rect_size.y, 0.0, vertical_margin)
+		current_child.margin_right = -horizontal_movement + (horizontal_movement/2)
+		current_child.margin_left = horizontal_movement - (horizontal_movement/2)
+		current_child.margin_top = vertical_movement - (vertical_movement/2)
+		current_child.margin_bottom = -vertical_movement + (vertical_movement/2)
+		continue
+	update_individual_margins()
 
-func edit_percent_margin(percent:float):
-	pass
+func update_individual_margins():
+	match currently_editing:
+		editing.H:
+			individual_margins["Left"] = horizontal_margin
+			individual_margins["Right"] = horizontal_margin
+		editing.V:
+			individual_margins["Top"] = vertical_margin
+			individual_margins["Bottom"] = vertical_margin
